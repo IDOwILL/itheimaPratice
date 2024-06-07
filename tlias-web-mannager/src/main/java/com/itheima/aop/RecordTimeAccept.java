bf47fc657c48aa21c3ee3ed9ac96fdf82334f4c3
IDOwILL <lzsimle@163.com>
2024-06-07 09:34:51 +0800

    aop基础

---
+++ tlias-web-mannager/src/main/java/com/itheima/aop/RecordTimeAccept.java
@@ -0,0 +1,29 @@
+package com.itheima.aop;
+
+import com.itheima.pojo.LoginInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class RecordTimeAccept {
+    /**
+     * 记录时间
+     *
+     * @param pjp pjp
+     * @return 对象
+     * @throws Throwable 可丢弃
+     */
+    @Around("execution(* com.itheima.service.impl.DeptServiceImpl.*(..) )")
+    public Object recordTime(ProceedingJoinPoint pjp) throws Throwable {
+        long begin = System.currentTimeMillis();
+        Object result = pjp.proceed();
+        long end = System.currentTimeMillis();
+        log.info("(end -begin) :{}",(end -begin));
+        return result;
+    }
+}
